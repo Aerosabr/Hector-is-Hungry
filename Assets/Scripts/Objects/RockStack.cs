9964841f93a094670c48a75b69ee245c1776abc6
Jordan <jhn100550@gmail.com>
2024-06-21 14:33:27 -0700

    Rock stacking

---
+++ Assets/Scripts/Objects/RockStack.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockStack : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> Rocks = new List<Sprite>();
+    [SerializeField] private List<GameObject> CachedRocks = new List<GameObject>();
+    [SerializeField] private SpriteRenderer StackOfRocks;
+    [SerializeField] private GameObject Built;
+    [SerializeField] private GameObject Unbuilt;
+    [SerializeField] private int numRocks;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Item" && numRocks < 3 && collision.gameObject.GetComponent<Rock>() != null && collision.gameObject.GetComponent<Item>().isDropped)
+        {
+            Debug.Log("Rock stacked");
+            numRocks++;
+            CachedRocks.Add(collision.gameObject);
+            collision.gameObject.SetActive(false);
+            collision.gameObject.GetComponent<Item>().region.numActive--;
+            StackOfRocks.sprite = Rocks[numRocks - 1];
+            if (numRocks == 3)
+            {
+                Built.SetActive(true);
+                Unbuilt.SetActive(false);
+            }
+        }
+    }
+
+}
